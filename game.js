87120e3fe52cb8559cbb3a9fac611544b5628404
sohamsinghal1211 <80926575+sohamsinghal1211@users.noreply.github.com>
2025-01-05 20:36:19 -0500

    Add files via upload

--- game.js
+++ game.js
@@ -1,52 +1,72 @@
-MouseEvent = "empty"
+var MouseEvent = "empty";
 var last_position_of_x, last_position_of_y;
-canvas = document.getElementById("Canvas")
-ctx = canvas.getContext("2d");
+var canvas = document.getElementById("Canvas");
+var ctx = canvas.getContext("2d");
 
+// Event to track mousedown
 canvas.addEventListener("mousedown", my_mousedown);
 
 function my_mousedown(e) {
-    color = document.getElementById("Color").value;
-    width_of_line = document.getElementById("line-width");
-    MouseEvent = "mouseDown";
+  color = document.getElementById("Color").value;  // Fetch the color value
+  width_of_line = document.getElementById("line-width").value;  // Fetch the line width value
+  MouseEvent = "mouseDown";  // Set event to mouseDown
+
+  // Update last_position_of_x and last_position_of_y to the current mouse position
+  last_position_of_x = e.clientX - canvas.offsetLeft;
+  last_position_of_y = e.clientY - canvas.offsetTop;
 }
 
-canvas.addEventListener("mousemove", my_mousemove)
+// Event to track mousemove
+canvas.addEventListener("mousemove", my_mousemove);
 
 function my_mousemove(e) {
-    current_position_of_x = e.clientX - canvas.offsetLeft
-    current_position_of_y = e.clientY - canvas.offsetTop
     if (MouseEvent == "mouseDown") {
+        var current_position_of_x = e.clientX - canvas.offsetLeft;
+        var current_position_of_y = e.clientY - canvas.offsetTop;
+        
         ctx.beginPath();
         ctx.strokeStyle = color;
         ctx.lineWidth = width_of_line;
-        console.log("Last position of x and y coordinates = ");
-        console.log("x = " + last_position_of_x + " " + "y = " + last_position_of_y);
+
+        console.log("Last position of x and y coordinates = ", last_position_of_x, last_position_of_y);
         ctx.moveTo(last_position_of_x, last_position_of_y);
-        console.log("Current position of x and y coordinates = ");
-        console.log("x = " + current_position_of_x + " " + "y = " + current_position_of_y);
+        
+        console.log("Current position of x and y coordinates = ", current_position_of_x, current_position_of_y);
         ctx.lineTo(current_position_of_x, current_position_of_y);
         ctx.stroke();
+
+        last_position_of_x = current_position_of_x;
+        last_position_of_y = current_position_of_y;
     }
-    last_position_of_x = current_position_of_x;
-    last_position_of_y = current_position_of_y;
 }
 
-function goBack2(){
-    window.location="navigate.html"
+// Event to stop drawing on mouseup
+canvas.addEventListener("mouseup", function() {
+    MouseEvent = "mouseUp";  // Stop drawing
+});
+
+// Event to handle mouse leaving the canvas
+canvas.addEventListener("mouseleave", function() {
+    MouseEvent = "mouseUp";  // Stop drawing
+});
+
+function goBack2() {
+    window.location = "navigate.html";
 }
 
 function onLoad() {
     gapi.load('auth2', function() {
-      gapi.auth2.init();
+        gapi.auth2.init();
     });
-  }
-  
-    function signOut() {
-      var auth2 = gapi.auth2.getAuthInstance();
-      auth2.signOut().then(function () {
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
         console.log('User signed out.');
-      });
-      window.location="index.html"
-  
-    }
+    });
+    window.location = "index.html";
+}
+function clearArea() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);  // Clear the entire canvas
+}
--- main.js
+++ main.js
@@ -1,72 +1,39 @@
-let user_name = '';
-let pass_word = '';
+user_name=''
+pass_word=''
 
-function signUp() {
-    // Get input values
-    user_name = document.getElementById("user_name").value;
-    pass_word = document.getElementById("password").value;
-
-    // Check if username or password is empty
-    if (user_name === "" || pass_word === "") {
-        alert("Username and Password can't be empty");
-        return;
+function signUp(){
+    user_name=document.getElementById("user_name").value;
+pwd=document.getElementById("password").value;
+    if(user_name=="" || pwd==""){
+alert("Username and Password cant be empty")
     }
 
-    // Validate password
-    if (!validatePassword(pass_word)) {
-        return; // Stop signup if password is invalid
-    }
-
-    alert("Signup successful!");
 }
-
-function validatePassword(password) {
-    if (password.length < 8) {
-        alert("Password must be at least 8 characters long.");
-        return false;
+function login(){
+    document.getElementById("msg").innerHTML="";
+    login_username=document.getElementById("login_username").value;
+    login_password=document.getElementById("login_password").value;
+    if(user_name != "" && pwd != "" && login_username != "" && login_password != ""){
+        if(user_name==login_username && pwd==login_password){
+            window.location="navigate.html"
+                }
+                else{
+            document.getElementById("msg").innerHTML="Invalid Username or Password"
+                }
     }
-    if (!/[A-Z]/.test(password)) {
-        alert("Password must contain at least one uppercase letter.");
-        return false;
-    }
-    if (!/[a-z]/.test(password)) {
-        alert("Password must contain at least one lowercase letter.");
-        return false;
-    }
-    if (!/[0-9]/.test(password)) {
-        alert("Password must contain at least one number.");
-        return false;
-    }
-    if (!/[!@#$%^&*(),.?\":{}|<>]/.test(password)) {
-        alert("Password must contain at least one special character.");
-        return false;
-    }
-    return true; // Password is valid
+  else{
+    document.getElementById("msg").innerHTML="Username and Password can not be Empty/Invalid"    
+  }
 }
-
-function login() {
-    document.getElementById("msg").innerHTML = "";
-
-    let login_username = document.getElementById("login_username").value;
-    let login_password = document.getElementById("login_password").value;
-
-    if (user_name !== "" && pass_word !== "" && login_username !== "" && login_password !== "") {
-        if (user_name === login_username && pass_word === login_password) {
-            window.location = "navigate.html";
-        } else {
-            document.getElementById("msg").innerHTML = "Invalid Username or Password";
-        }
-    } else {
-        document.getElementById("msg").innerHTML = "Username and Password cannot be Empty/Invalid";
-    }
-}
-
 function onSignIn(googleUser) {
-    // Handle Google Sign-In
+    // alert("sign in");
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    window.location = "navigate.html";
-}
+    window.location="navigate.html"
+
+       
+  }
+  
